c7e0a49f832490d542e8477a3bbe604dc2d39ee0
josar <josarroliga0943@outlook.es>
2023-03-29 01:13:15 -0600

    android terminado

---
+++ app/src/main/java/com/example/proyectopruebaxd/Coordinador.kt
@@ -0,0 +1,9 @@
+package com.example.proyectopruebaxd
+
+data class Coordinador(val idC: Int,
+                       val nombres: String,
+                       val apellidos: String,
+                       val fechaNac: String,
+                       val titulo: String,
+                       val email: String,
+                       val facultad: String)
