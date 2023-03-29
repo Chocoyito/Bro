7045723201bdd45590f2a678c1cdb051e60e0f35
josar <josarroliga0943@outlook.es>
2023-03-28 21:43:37 -0600

    nada mas

--- mostrar_coordinador.php
+++ mostrar_coordinador.php
@@ -1,7 +1,8 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "GET") {
     require_once 'Conexion.php';
-    $my_query = "SELECT * FROM coordinador";
+    $my_query = "SELECT * FROM coordinador
+    WHERE DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), fechaNac)), '%Y')+0 >= 60;";
     
     $result = $mysql ->query($my_query);
     if($mysql->affected_rows > 0) {
